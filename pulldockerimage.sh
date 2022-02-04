5a12c6d441ab1e62f7d3587240e111e636a41a11
salat23 <misikon@mail.ru>
2022-02-04 22:25:37 +0300

    added docker compose script

---
+++ pulldockerimage.sh
@@ -0,0 +1,12 @@
+git pull https://salat-23:"${GITHUB_TOKEN}"@https://github.com/salat-23/wafflesproject.git
+
+mvn clean
+mvn package
+
+cp target/wafflesproject-0.0.1-SNAPSHOT.jar src/main/docker
+
+cd src/main/docker
+
+docker-compose build
+docker-compose pull
+docker-compose up
