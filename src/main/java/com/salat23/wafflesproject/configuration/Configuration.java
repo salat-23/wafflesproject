5797a5ab89a00e45d6610775b17969bd675af2ce
salat-23 <misikon@mail.ru>
2022-02-09 00:12:21 +0300

    added ssl support

---
+++ src/main/java/com/salat23/wafflesproject/configuration/Configuration.java
@@ -0,0 +1,20 @@
+package com.salat23.wafflesproject.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .requiresChannel(channel ->
+                        channel.anyRequest().requiresSecure())
+                .authorizeRequests(authorize ->
+                        authorize.anyRequest().permitAll())
+                .build();
+    }
+
+}
